55393ccf3a1cb5fb624cb5326ce25dee413b4ec9
VICTOR LLORENTE <vllorente@proinceringenieros.com>
2026-02-06 12:28:07 +0100

    Pantalla Login terminada

---
+++ MINILIGA/src/main/java/com/proinceringenieros/miniliga/LoginController.java
@@ -0,0 +1,24 @@
+package com.proinceringenieros.miniliga;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField tfUsuario;
+    @FXML
+    private PasswordField pfPasword;
+    @FXML
+    private Button btnLogin;
+    @FXML
+    private Label lblMensaje;
+    @FXML
+    private Button btnRegistrar;
+
+
+
+}
